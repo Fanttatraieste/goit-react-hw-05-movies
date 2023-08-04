864b45ec118f2945db9c56d9113a827223c1af80
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-08-04 16:19:22 +0300

    rezolvata prob cu cheia

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,63 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const API_KEY = process.env.API_KEY;
+const key = process.env.REACT_APP_API_KEY;
+// console.log(process.env.REACT_APP_API_KEY);
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${key}`,
+  },
+};
 
 export default function Home() {
   const [isLoading, setLoading] = useState(true);
+  const [movieList, setMovieList] = useState({});
+
+  // fetch(
+  //   'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+  //   options
+  // )wponse.json())
+  //   .then(response => {
+  //     console.log(response);
+  //     setMovieList(response);
+  //   })
+  //   .catch(err => console.error(err));
+
+  useEffect(() => {
+    setMovieList([]);
+    fetch(
+      'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+      options
+    )
+      .then(response => response.json())
+      .then(response => {
+        // console.log(response);
+        // console.log(key);
+        setMovieList(response.results);
+      })
+      .catch(err => console.error(err))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return (
-    <div>
+    <div style={{ padding: '50px 80px' }}>
       <h2>Trending today</h2>
       {isLoading && <h3>The trending movies are loading</h3>}
+      {!isLoading && <MovieList movies={movieList} />}
+    </div>
+  );
+}
+
+function MovieList({ movies }) {
+  return (
+    <div>
+      {movies.map((movie, index) => (
+        <p key={index}>{movie.title}</p>
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import './index.css';
 import App from 'App';
 import { BrowserRouter } from 'react-router-dom';
 
+// const dotenv = require('dotenv');
+// dotenv.config({ path: '../config.env' });
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
